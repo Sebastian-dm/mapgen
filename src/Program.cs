a835db66e26527e973957c0ac81c282f67483322
Sebastian Meier <sebastiandmeier@gmail.com>
2023-02-01 10:46:29 +0100

    Add binary read

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+
+string path = @"C:\Users\sdme\git\mapgen\res\GTOPO30\all10\a10g";
+byte[] readText = File.ReadAllBytes(path);
+
+
+List<Int16> uniqueNumbers = new();
+
+for (int i = 0; i < readText.Length/2; i++) {
+    Int16 number = BitConverter.ToInt16(readText, i*2);
+    if (!uniqueNumbers.Contains(number)) {
+        uniqueNumbers.Add(number);
+        Console.WriteLine(number);
+    }
+    
+}
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Finished");
