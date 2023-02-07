49fe6dce180282f0ee25e1e0ea1546895751bb13
Sebastian Meier <sebastiandmeier@gmail.com>
2023-02-07 21:38:00 +0100

    Add method to find tile from lat lon

--- src/ElevationModel.cs
+++ src/ElevationModel.cs
@@ -5,206 +5,56 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace mapgen {
-    internal class ElevationModel {
+    internal class ElevationModelGTOPO30
+    {
+
+        private string _tilePath = "C:/Users/sdme/git/mapgen/res/GTOPO30/all10/";
+        private string _tilePrefix = "a10";
+        private char[,] _tileChars = new char[4, 4] {
+            { 'A', 'B', 'C', 'D' },
+            { 'E', 'F', 'G', 'H' },
+            { 'I', 'J', 'K', 'L' },
+            { 'M', 'N', 'O', 'P' }
+        };
 
 
-
-        private string GetNameOfTile(float lat, float lon) {
-
-            string tile = "";
-            switch (lat) {
-                case > 90:
-                    throw new ArgumentException("input latitude cannot be above 90");
-                case > 50:
-                    switch (lon) {
-                        case > 180:
-                            throw new ArgumentException("input longitude cannot be above 180");
-                        case > 90:
-                            tile = "D";
-                            break;
-                        case > 0:
-                            tile = "C";
-                            break;
-                        case > -90:
-                            tile = "B";
-                            break;
-                        case >= -180:
-                            tile = "A";
-                            break;
-                        default:
-                            throw new ArgumentException("input longitude cannot be below -180");
-                    }
-                    break;
-                case > 0:
-                    switch (lon) {
-                        case > 180:
-                            throw new ArgumentException("input longitude cannot be above 180");
-                        case > 90:
-                            tile = "";
-                            break;
-                        case > 0:
-                            tile = "G";
-                            break;
-                        case > -90:
-                            tile = "F";
-                            break;
-                        case >= -180:
-                            tile = "E";
-                            break;
-                        default:
-                            throw new ArgumentException("input longitude cannot be below -180");
-                    }
-                    break;
-                case > -50:
-                    switch (lon) {
-                        case > 180:
-                            throw new ArgumentException("input longitude cannot be above 180");
-                        case > 90:
-                            tile = "";
-                            break;
-                        case > 0:
-                            tile = "";
-                            break;
-                        case > -90:
-                            tile = "";
-                            break;
-                        case >= -180:
-                            tile = "";
-                            break;
-                        default:
-                            throw new ArgumentException("input longitude cannot be below -180");
-                    }
-                    break;
-                case >= -90:
-                    switch (lon) {
-                        case > 180:
-                            throw new ArgumentException("input longitude cannot be above 180");
-                        case > 90:
-                            tile = "";
-                            break;
-                        case > 0:
-                            tile = "";
-                            break;
-                        case > -90:
-                            tile = "";
-                            break;
-                        case >= -180:
-                            tile = "";
-                            break;
-                        default:
-                            throw new ArgumentException("input longitude cannot be below -180");
-                    }
-                    break;
-                default:
-                    throw new ArgumentException("input latitude cannot be below -90");
-            }
+        private string GetTileName(float lat, float lon) {
+            int[] tileIndex = GetTileIndex(lat, lon);
+            string tileName = _tilePrefix + _tileChars[tileIndex[0], tileIndex[1]];
+            return tileName;
         }
-            {
-                "Tiles": {
-                    "A": {
-                        "LatMin": 50,
-            "LatMax": 90,
-            "LonMin": -180,
-            "LonMax": -90,
-                    },
-        "B": {
-                        "LatMin": 50,
-            "LatMax": 90,
-            "LonMin": 90,
-            "LonMax": 0,
-        },
-        "C": {
-                        "LatMin": 50,
-            "LatMax": 90,
-            "LonMin": 0,
-            "LonMax": 90,
-        },
-        "D": {
-                        "LatMin": 50,
-            "LatMax": 90,
-            "LonMin": 90,
-            "LonMax": 180,
-        },
-        "E": {
-                        "LatMin": 0,
-            "LatMax": 50,
-            "LonMin": -180,
-            "LonMax": -90,
-        },
-        "F": {
-                        "LatMin": 0,
-            "LatMax": 50,
-            "LonMin": -90,
-            "LonMax": 0,
-        },
-        "G": {
-                        "LatMin": 0,
-            "LatMax": 50,
-            "LonMin": 0,
-            "LonMax": 90,
-        },
-        "H": {
-                        "LatMin": 0,
-            "LatMax": 50,
-            "LonMin": 90,
-            "LonMax": 180,
-        },
-        "I": {
-                        "LatMin": -50,
-            "LatMax": 0,
-            "LonMin": -180,
-            "LonMax": -90,
-        },
-        "J": {
-                        "LatMin": -50,
-            "LatMax": 0,
-            "LonMin": -90,
-            "LonMax": 0,
-        },
-        "K": {
-                        "LatMin": -50,
-            "LatMax": 0,
-            "LonMin": 0,
-            "LonMax": 90,
-        },
-        "L": {
-                        "LatMin": -50,
-            "LatMax": 0,
-            "LonMin": 90,
-            "LonMax": 180,
-        },
-        "L": {
-                        "LatMin": -50,
-            "LatMax": 0,
-            "LonMin": 90,
-            "LonMax": 180,
-        },
-        "M": {
-                        "LatMin": -90,
-            "LatMax": -50,
-            "LonMin": -180,
-            "LonMax": -90,
-        },
-        "N": {
-                        "LatMin": -90,
-            "LatMax": -50,
-            "LonMin": -90,
-            "LonMax": 0,
-        },
-        "O": {
-                        "LatMin": -90,
-            "LatMax": -50,
-            "LonMin": 0,
-            "LonMax": 90,
-        },
-        "P": {
-                        "LatMin": -90,
-            "LatMax": -50,
-            "LonMin": 90,
-            "LonMax": 180,
+
+        private int[] GetTileIndex(float lat, float lon) {
+
+        if (lon < -180 || lon > 180)
+            throw new ArgumentException($"Input latitude must be between -180 and 180. Received {lat}");
+        if (lat < -90 || lat > 90)
+            throw new ArgumentException($"Input longitude must be between -90 and 90. Received {lon}");
+
+        int[] tileIndex = new int[2] { -1, -1 };
+
+        switch (lon) {
+            case < -90:
+                tileIndex[0] = 0; break;
+            case < 0:
+                tileIndex[0] = 1; break;
+            case < 90:
+                tileIndex[0] = 2; break;
+            default:
+                tileIndex[0] = 3; break;
         }
-                }
-            }
+        switch (lat) {
+            case < -50:
+                tileIndex[1] = 0; break;
+            case < 0:
+                tileIndex[1] = 1; break;
+            case < 50:
+                tileIndex[1] = 2; break;
+            default:
+                tileIndex[1] = 3; break;
+        }
+
+        return tileIndex;
         }
 
 
